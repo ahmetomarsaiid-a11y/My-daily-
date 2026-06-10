c29a572e518b50425e27706a3d33187e9c1f648a
ahmetomarsaiid-a11y <ahmetomarsaiid@gmail.com>
2026-06-10 12:48:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(level
 logger = logging.getLogger(__name__)
 
 async def main():
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN)    
 dp = Dispatcher(storage=MemoryStorage())
 
 DB_PATH = os.getenv("DB_PATH", "/app/data/movies.db")
